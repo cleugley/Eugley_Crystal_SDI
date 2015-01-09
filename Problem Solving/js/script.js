d2c7b9c0e416663fdc5744360fe6aa7089b3171d
cleugley <cleugley@fullsail.edu>
2015-01-08 21:38:58 -0700

    Added Variable and User Prompt
    Added Variable and User Prompt

--- Problem Solving/js/script.js
+++ Problem Solving/js/script.js
@@ -6,3 +6,9 @@ Go To Training Week #1
  */
 
 alert ("Testing to make sure its connected.");
+
+console.log ("This is the console.");
+
+var userInput = prompt ("Please type in your name");
+
+console.log("Welcome "+userInput);
