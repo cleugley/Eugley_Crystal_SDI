d6af778be08090634559c14241c3a112cabec0c3
cleugley <cleugley@fullsail.edu>
2015-01-14 10:05:44 -0700

    Defining Variables Video
    Defining Variables Video

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,3 +1,7 @@
-// Java Script
+// Defining Variables
 
-var age;
+var age; //declaration
+age = 38; //definition
+//storage container = value
+
+var yearOfBirth = 1970; //declaring and defining on the same line
