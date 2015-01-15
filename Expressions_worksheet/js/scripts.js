660e2ecde4968e705a543866108d1eccf7e69e67
cleugley <cleugley@fullsail.edu>
2015-01-15 13:43:47 -0700

    Problem 1 Dog Years
    Problem 1 Dog Years

--- Expressions_worksheet/js/scripts.js
+++ Expressions_worksheet/js/scripts.js
@@ -1,3 +1,25 @@
 /**
  * Created by Ceugley on 1/15/15.
  */
+// Crystal Eugley, 1/15/2015, Expressions_Worksheet
+
+//Calculate the area of a rectangle given the width and height of the rectangle
+var width = 10;
+var height = 5;
+
+// results for area of a rectangle is width times height
+var area = width * height;
+
+console.log("The area of the rectangle is" + " " + area);
+
+// Problem: Dog Years
+// Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in "dog years." Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+var sparkysAge = 1;
+var humanYears = 7;  //Number of years to one dog year
+
+var age = sparkysAge * humanYears;
+
+console.log("Sparky is " + age + " " + "human years old which is " + sparkysAge + " " + "in dog years.");
+
+
