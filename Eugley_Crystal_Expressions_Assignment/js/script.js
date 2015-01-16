70bd5a5626dee2adf65dfd8b28da34371ecdf4fc
cleugley <cleugley@fullsail.edu>
2015-01-15 18:50:39 -0700

    Figuring out formulas
    Figuring out Formulas

--- Eugley_Crystal_Expressions_Assignment/js/script.js
+++ Eugley_Crystal_Expressions_Assignment/js/script.js
@@ -5,9 +5,15 @@
 var dogFoodPrice = prompt("Enter the price of your dog food at store 1");  //Cost of one bag of dog food
 var foodDiscount = prompt("Enter the discount percentage as a whole number"); //The amount of discount store 1 is offering
 
-var totalPrice = dogFoodPrice - dogFoodPrice/foodDiscount; //The total cost of the dog food after the discount being offered
+var totalPrice = dogFoodPrice - foodDiscount*dogFoodPrice/100 ; //The total cost of the dog food after the discount being offered
 
 alert("Your total cost before tax is "+totalPrice);
 
 var dogFoodPrice2 = prompt("Enter the price of your dog food at store 2"); //Enter the price of dog food again
 var foodDiscount2 = prompt("Enter the discount percentage as a whole number"); //Enter the price of dog food being offered at Store 2
+
+var totalPrice2 = dogFoodPrice2 - foodDiscount2*dogFoodPrice2/100;
+
+alert("Your total cost before tax at store 2 is "+totalPrice2);
+
+alert ("Your before tax price at Store 1 is " + totalPrice + "Your before tax price at Store 2 is " + totalPrice2);
