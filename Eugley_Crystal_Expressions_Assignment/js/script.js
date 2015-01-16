87a2f4d9087b1f264322a167bb08ffccbadd1f0e
cleugley <cleugley@fullsail.edu>
2015-01-15 18:52:56 -0700

    Adding comments to lines
    Adding comments to lines

--- Eugley_Crystal_Expressions_Assignment/js/script.js
+++ Eugley_Crystal_Expressions_Assignment/js/script.js
@@ -7,13 +7,13 @@ var foodDiscount = prompt("Enter the discount percentage as a whole number"); //
 
 var totalPrice = dogFoodPrice - foodDiscount*dogFoodPrice/100 ; //The total cost of the dog food after the discount being offered
 
-alert("Your total cost before tax is "+totalPrice);
+alert("Your total cost before tax is "+totalPrice); //Stating the total cost before taxes at Store 1
 
 var dogFoodPrice2 = prompt("Enter the price of your dog food at store 2"); //Enter the price of dog food again
 var foodDiscount2 = prompt("Enter the discount percentage as a whole number"); //Enter the price of dog food being offered at Store 2
 
-var totalPrice2 = dogFoodPrice2 - foodDiscount2*dogFoodPrice2/100;
+var totalPrice2 = dogFoodPrice2 - foodDiscount2*dogFoodPrice2/100; //The total cost of the dog food after the discount being offered at Store 2
 
-alert("Your total cost before tax at store 2 is "+totalPrice2);
+alert("Your total cost before tax at store 2 is "+totalPrice2); //Total Price before taxes at Store will be displayed
 
-alert ("Your before tax price at Store 1 is " + totalPrice + "Your before tax price at Store 2 is " + totalPrice2);
+alert ("Your before tax price at Store 1 is " + totalPrice + "Your before tax price at Store 2 is " + totalPrice2); //This will display a comparison of prices for Store 1 and Store 2
