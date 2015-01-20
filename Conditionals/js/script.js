91f54158e99055e1b0c502d0bd254c1c0b80cf5f
cleugley <cleugley@fullsail.edu>
2015-01-19 20:26:11 -0700

    Screencast: Ternary Operators
    Screencast: Ternary Operators

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,46 +1,19 @@
 /**
  * Created by Ceugley on 1/19/15.
  */
-//Conditional Logic
 
-//var oldEnough = false;
+// conditional logic - ternary operators
+var age = 6;
+var book;
 
-/*If child is old enough, print to the console "you can ride!"
-if(oldEnough){
-   // code performed if condition is true
-    console.log("You can ride the coaster!");
-}*/
+// If the child is under 10, they get Green Eggs and Ham, otherwise they get the Time Machine
 
-
-// Conditional Logic - Relational Expressions
-/*var kidHeight = 47;
-var minHeight = 48;
-var sneakerLift = 2;
-// If the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
-    console.log("You can ride the coaster!");
-
-}
-
-var kidHeight = 49;
-var minHeight = 48;
-
-if (kidHeight > minHeight){
-    console.log("You can ride the coaster!");
-} else{
-    console.log("Sorry kid, you've got some growing to do first!");
-} */
-
-// Conditional Logic - Logical Operators
-
-var budget = 300;
-var iPhonePrice = 199.99;
-var wonLottery = true;
-
-//if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget ^ wonLottery === true){
-    console.log("We can buy the phone!");
+if(age <10){
+    book = "Green Eggs and Ham";
 }else{
-    console.log("No Phone for you!");
+    book = "The Time Machine";
 }
+console.log(book);
 
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
