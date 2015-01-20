826c283f6d414b8ebc1e951b47881dd0cda137bc
cleugley <cleugley@fullsail.edu>
2015-01-19 19:29:46 -0700

    Screencast: Conditionals Example
    Screencast: Conditional Example

---
+++ Conditionals/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ceugley on 1/19/15.
+ */
+//Conditional Logic
+
+var oldEnough = false;
+
+//If child is old enough, print to the console "you can ride!"
+if(oldEnough){
+   // code performed if condition is true
+    console.log("You can ride the coaster!");
+}
