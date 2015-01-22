6c3f24808857351488560ad5c295340b2da5d094
cleugley <cleugley@fullsail.edu>
2015-01-22 14:58:14 -0700

    prompt validation added
    prompt validation added

--- Eugley_Crystal_Conditionals_Assignment/js/script.js
+++ Eugley_Crystal_Conditionals_Assignment/js/script.js
@@ -6,8 +6,27 @@ Conditionals Assignment
 
 
 var age = prompt("Please enter your age");
+if(isNaN(age)){
+    age = prompt("Please only enter numbers. What is your age!");
+}
+if(age === ""){
+    age = prompt("Again, Please enter your age!")
+}
+
 var height = prompt("Please enter your height in inches");
+if(isNaN(height)){
+    height = prompt("Please only enter numbers. What is your age!");
+}
+if(height === ""){
+    height = prompt("Again, Please enter your age!")
+}
 var weight = prompt("Please enter your weight in pounds");
+if(isNaN(weight)){
+    weight = prompt("Please only enter numbers. What is your age!");
+}
+if(weight === ""){
+    weight = prompt("Again, Please enter your age!")
+}
 
 if(age < 21){
     console.log("You may NOT enter the bar! You are not old enough");
