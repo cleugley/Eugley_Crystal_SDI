c4d462b1f8a716da8c04c8d69c6b1c6973b176b7
cleugley <cleugley@fullsail.edu>
2015-01-26 09:45:30 -0700

    Screencast Functions vs Procedures
    Screencast Functions vs Procedures

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -3,13 +3,17 @@
  */
 
 
-// Returning Values
+// Procedures
 
-var total = calcArea (30, 20);
-
-function calcArea(w, h){
-    var area = w * h;
-    return area;  //function is spitting the info out, but must be stored somewhere
+//this is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area;
 }
-console.log(total);
+
+//this is a procedure
+function calcArea(width, height){
+    var area = width * height;
+    console.log(area); //no return
+}
 
