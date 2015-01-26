b4a4480c4634c6ed49250b97cdf83483eecda3a3
cleugley <cleugley@fullsail.edu>
2015-01-26 09:36:53 -0700

    Screencast vid: Arguments & Parameters
    Arguments and parameters

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,16 +1,22 @@
 /**
  * Created by Ceugley on 1/26/15.
  */
+// Arguments and Parameters
 
-var width = 5;
+calcArea(30, 20);
 
 
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
+function calcArea(w, h){ // w=30, h=20
+  var area =w * h;
     console.log(area);
 }
 
-calcArea();
-console.log(width);
+
+//how old is my dog in human years
+
+function dogYears(age /*holds parameters*/){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old")
+}
+dogYears(4/*holds definition for the parameter i.e. arguments*/);
+dogYears(5);
