b9bf1233f18629a1084a2db2bd8b4117d9a83c5e
cleugley <cleugley@fullsail.edu>
2015-01-26 09:25:25 -0700

    Variable Scope
    Functions Variable Scope video

---
+++ Functions/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ceugley on 1/26/15.
+ */
+
+var width = 5;
+
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea();
+console.log(width);
