050ca9ea8dcfd34356104d83e4cecdd642db9c7f
cleugley <cleugley@fullsail.edu>
2015-01-26 10:51:51 -0700

    While loop
    while loop screencast

---
+++ Loops/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ceugley on 1/26/15.
+ */
+//Screencast: loops
+
+var b = 10; //setups the index
+
+while (b > 0){ //checks the condition
+    console.log(b + " kegs on the wall");
+    b--; // increments or decrements the index
+}
