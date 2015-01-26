01b8ade3698ad432fb12cab61f00feecf670fe23
cleugley <cleugley@fullsail.edu>
2015-01-26 14:15:04 -0700

    code setup
    code setup

--- Eugley_Crystal_Functions_Assignment/js/script.js
+++ Eugley_Crystal_Functions_Assignment/js/script.js
@@ -4,3 +4,27 @@
 // Crystal Eugley
 // 1/26/2015
 // Functions Assignment
+
+// Must have two working functions one normal and one anonymous
+// Arguments & Parameters are present and working in every function
+// Return values are present and working in each of the functions
+// At least one of the examples contains a function that has 3 parameters
+// Main code variables are not used inside of functions
+// At least 3 user prompts in code that are all correctly validated
+
+// We will do calculations for a person who is looking to buy a house.
+
+var listPrice = prompt("Please enter the $ amount on the listing price");
+var intRate = prompt ("Please enter the interest rate of your loan. (Please enter a whole number. ex. 6.25% you would put .0625");
+var loanTerm = prompt ("Please enter the terms of the loan. (i.e. How many years you are financing for");
+
+function monthlyPayment(listPrice, intRate, loanTerm){
+    var moPay = listPrice/loanTerm * intRate;
+    return moPay;
+}
+
+
+// actual formula for mortgage payment calculation
+// M=P i(1+i)n/(1+i)n-1
+//M = monthly payment P = principal i=interest rate n=number of payments
+
