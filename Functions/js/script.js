44739e073ff1a9eaaf171312c05aae45bbed1385
cleugley <cleugley@fullsail.edu>
2015-01-26 09:42:32 -0700

    Screencast Returning Values
    Screencast Returning Values

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,22 +1,15 @@
 /**
  * Created by Ceugley on 1/26/15.
  */
-// Arguments and Parameters
-
-calcArea(30, 20);
 
 
-function calcArea(w, h){ // w=30, h=20
-  var area =w * h;
-    console.log(area);
+// Returning Values
+
+var total = calcArea (30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area;  //function is spitting the info out, but must be stored somewhere
 }
+console.log(total);
 
-
-//how old is my dog in human years
-
-function dogYears(age /*holds parameters*/){
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + " years old")
-}
-dogYears(4/*holds definition for the parameter i.e. arguments*/);
-dogYears(5);
