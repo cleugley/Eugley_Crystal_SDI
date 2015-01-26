576fe4c2b102431fe4c728c05b90d8e294899344
cleugley <cleugley@fullsail.edu>
2015-01-26 13:20:45 -0700

    loop screen cast videos
    do while, while and for loops

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -1,11 +1,27 @@
 /**
  * Created by Ceugley on 1/26/15.
  */
-//Screencast: loops
+//Screencast: While loops
 
 var b = 10; //setups the index
 
 while (b > 0){ //checks the condition
     console.log(b + " kegs on the wall");
     b--; // increments or decrements the index
+}
+
+console.log('.......Do While loops........');
+
+var c = 10;
+
+do{
+    console.log(c + ' kegs on the wall.');
+    c--;
+}
+while (c > 0);
+
+console.log('...............For Loops................');
+
+for (var i = 10; i > 0; i--){
+    console.log(i + ' kegs on the wall.');
 }
